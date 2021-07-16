e045e76c5732201dfa0c976e4147a3a80ac689cc
liuyuancheng <tenstone@foxmail.com>
2021-07-16 11:47:20 +0800

    splitting single project named platform to many modules.

---
+++ platform-eventbus/src/main/java/com/bosuyun/platform/api/DashboardService.java
@@ -0,0 +1,13 @@
+package com.bosuyun.platform.api;
+
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ * dashboard 首页可定制内容模块
+ *
+ * Created by liuyuancheng on 2021/1/21  <br/>
+ */
+@ApplicationScoped
+public class DashboardService {
+
+}
---
+++ platform-eventbus/src/main/java/com/bosuyun/platform/api/MenuService.java
@@ -0,0 +1,13 @@
+package com.bosuyun.platform.api;
+
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ * 通用菜单管理
+ *
+ * Created by liuyuancheng on 2021/1/21  <br/>
+ */
+@ApplicationScoped
+public class MenuService {
+
+}
